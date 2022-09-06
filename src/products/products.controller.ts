6dde1e4c8f8685fa34834dc78e56dc0f19b9291b
Hendro  Setiawan <hendrosetiawan@Asanis-MacBook-Pro-2.local>
2022-09-06 08:06:32 +0700

    crud api

---
+++ src/products/products.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ProductEntity } from './entities/product.entity';
+
+@Controller('products')
+@ApiTags('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: ProductEntity, isArray: true })
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get('drafts')
+  findDrafts() {
+    return this.productsService.findDrafts();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
+  }
+}
